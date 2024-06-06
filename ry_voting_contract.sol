7e041f18fa5f6e4e56f7d21cc8256e7212e669ba
Ravi Yeleswarapu <149740090+ravyeles@users.noreply.github.com>
2024-06-06 18:28:37 -0500

    commit

--- ry_voting_contract.sol
+++ ry_voting_contract.sol
@@ -2,7 +2,7 @@
 Decentralized Voting Platform
 */
 
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
